a8564de31ebb3f2494fac77cbeb5d58b1e3a957a
YuiCheng <55050523+yuicheng1006@users.noreply.github.com>
2023-12-26 14:42:04 +0800

    Update all.js

--- all.js
+++ all.js
@@ -4,31 +4,59 @@ const domain = location.href;
 console.log('domain', domain);
 
 btn.addEventListener('click', async () => {
-  const formData = new FormData();
-  formData.append('MerchantID', '2000132');
-  formData.append('LogisticsType', 'CVS');
-  formData.append('LogisticsSubType', 'UNIMART');
-  formData.append('IsCollection', 'Y');
-  formData.append('ServerReplyURL', domain);
+  // WARNING: For POST requests, body is set to null by browsers.
+  var data =
+    'MerchantID=2000132&LogisticsType=CVS&LogisticsSubType=UNIMART&IsCollection=Y&ServerReplyURL=https%3A%2F%2Fyuicheng1006.github.io%2Ftestecpay%2F';
 
-  try {
-    const res = await axios.post(
-      // 'https://test-ec2.supermedia.cool/api/products',
-      'https://logistics-stage.ecpay.com.tw/Express/map',
-      formData,
-      {
-        headers: {
-          Accept: 'text/html',
-          ContentType: 'application/x-www-form-urlencoded',
-        },
-      },
-    );
-    console.log('res', res);
-  } catch (error) {
-    console.log('error', error);
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === 4) {
+      console.log(this.responseText);
+    }
+  });
+
+  xhr.open('POST', 'https://logistics-stage.ecpay.com.tw/Express/map');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // WARNING: Cookies will be stripped away by the browser before sending the request.
+  xhr.setRequestHeader(
+    'Cookie',
+    'MapInfo=MerchantID=2000132&MerchantTradeNo=ECPay&LogisticsType=CVS&LogisticsSubType=UNIMART&IsCollection=Y&ServerReplyURL=https%3a%2f%2fyuicheng1006.github.io%2ftestecpay%2f&CallBackFunction=&IsGet=&Device=0',
+  );
+
+  xhr.send(data);
 });
 
+// const btn = document.querySelector('button');
+
+// const domain = location.href;
+// console.log('domain', domain);
+
+// btn.addEventListener('click', async () => {
+//   const formData = new FormData();
+//   formData.append('MerchantID', '2000132');
+//   formData.append('LogisticsType', 'CVS');
+//   formData.append('LogisticsSubType', 'UNIMART');
+//   formData.append('IsCollection', 'Y');
+//   formData.append('ServerReplyURL', domain);
+
+//   try {
+//     const res = await axios.post(
+//       'https://logistics-stage.ecpay.com.tw/Express/map',
+//       formData,
+//       {
+//         headers: {
+//           Accept: 'text/html',
+//           ContentType: 'application/x-www-form-urlencoded',
+//         },
+//       },
+//     );
+//     console.log('res', res);
+//   } catch (error) {
+//     console.log('error', error);
+//   }
+// });
 
 // const domain = location.href;
 // console.log('domain', domain);
@@ -55,5 +83,3 @@ btn.addEventListener('click', async () => {
 // };
 
 // document.querySelector('button').addEventListener('click', getMapHandler);
-
-
