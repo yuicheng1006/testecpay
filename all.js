75430bfd6c8d53e371a627f4c406b841d5ead63c
YuiCheng <55050523+yuicheng1006@users.noreply.github.com>
2023-12-26 14:56:56 +0800

    Update all.js

--- all.js
+++ all.js
@@ -1,27 +1,30 @@
-const btn = document.querySelector('button');
 
 const domain = location.href;
 console.log('domain', domain);
 
-btn.addEventListener('click', async () => {
-  // WARNING: For POST requests, body is set to null by browsers.
-  var data =
-    'MerchantID=2000132&LogisticsType=CVS&LogisticsSubType=UNIMART&IsCollection=Y&ServerReplyURL=https%3A%2F%2Fyuicheng1006.github.io%2Ftestecpay%2F';
+const createHiddenInput = (form, name, value) => {
+  const hiddenField = document.createElement('input');
+  hiddenField.type = 'hidden';
+  hiddenField.name = name;
+  hiddenField.value = value;
+  form.appendChild(hiddenField);
+};
 
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
+const getMapHandler = () => {
+  const form = document.createElement('form');
+  form.method = 'POST';
+  form.enctype = 'application/x-www-form-urlencoded';
+  form.action = 'https://logistics-stage.ecpay.com.tw/Express/map'; // 這是測試的網址，文件上會寫正式的是哪個網址
+  createHiddenInput(form, 'MerchantID', '2000132');
+  createHiddenInput(form, 'LogisticsType', 'CVS');
+  createHiddenInput(form, 'LogisticsSubType', 'UNIMART');
+  createHiddenInput(form, 'IsCollection', 'Y');
+  createHiddenInput(form, 'ServerReplyURL', domain);
+  document.body.appendChild(form);
+  form.submit();
+};
 
-  xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
-  });
-
-  xhr.open('POST', 'https://logistics-stage.ecpay.com.tw/Express/map');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  xhr.send(data);
-});
+document.querySelector('button').addEventListener('click', getMapHandler);
 
 // const btn = document.querySelector('button');
 
@@ -53,28 +56,3 @@ btn.addEventListener('click', async () => {
 //   }
 // });
 
-// const domain = location.href;
-// console.log('domain', domain);
-
-// const createHiddenInput = (form, name, value) => {
-//   const hiddenField = document.createElement('input');
-//   hiddenField.type = 'hidden';
-//   hiddenField.name = name;
-//   hiddenField.value = value;
-//   form.appendChild(hiddenField);
-// };
-
-// const getMapHandler = () => {
-//   const form = document.createElement('form');
-//   form.method = 'POST';
-//   form.action = 'https://logistics-stage.ecpay.com.tw/Express/map'; // 這是測試的網址，文件上會寫正式的是哪個網址
-//   createHiddenInput(form, 'MerchantID', '2000132');
-//   createHiddenInput(form, 'LogisticsType', 'CVS');
-//   createHiddenInput(form, 'LogisticsSubType', 'UNIMART');
-//   createHiddenInput(form, 'IsCollection', 'Y');
-//   createHiddenInput(form, 'ServerReplyURL', domain);
-//   document.body.appendChild(form);
-//   form.submit();
-// };
-
-// document.querySelector('button').addEventListener('click', getMapHandler);
